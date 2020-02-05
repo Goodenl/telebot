a3a7acb038f63c4a6f5c305f469e6be551a802d8
Goodenl <34214638+Goodenl@users.noreply.github.com>
2020-02-05 23:02:51 +0200

    Our project AND correct deleting message

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
+project = ['World-and-excellence','polskapizda','telegram channel']
+
 @bot.message_handler(commands=['start'])
 def stiker_Hi(message):
 	sti = open('stickers/sticker.webp', 'rb')
@@ -15,7 +17,7 @@ def stiker_Hi(message):
 	# keyboard
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	item1 = types.KeyboardButton('Рэндоме')
-	item2 = types.KeyboardButton('Як справи, хлопче?')
+	item2 = types.KeyboardButton('Наши проекты')
 	item3 = types.KeyboardButton('Слитая инфаа!')
 
 	markup.add(item1, item2, item3)
@@ -28,15 +30,16 @@ def pressMarkup(message):
 	if message.chat.type == 'private':
 		if message.text == 'Рэндоме':
 			bot.send_message(message.chat.id, str(random.randint(0,100)))
-		elif message.text == 'Як справи, хлопче?':
+		elif message.text == 'Наши проекты':
 
 			markup = types.InlineKeyboardMarkup(row_width = 2)
-			item1 = types.InlineKeyboardButton("Хорошо", callback_data="good")
-			item2 = types.InlineKeyboardButton("Чёт умерать пора!", callback_data="bad")
+			item1 = types.InlineKeyboardButton(project[0], callback_data=project[0])
+			item2 = types.InlineKeyboardButton(project[1], callback_data=project[1])
+			item3 = types.InlineKeyboardButton("Уйди", callback_data="close")
 
-			markup.add(item1, item2)
+			markup.add(item1, item2, item3)
 
-			bot.send_message(message.chat.id, 'Добре! Як сам?', reply_markup=markup)
+			bot.send_message(message.chat.id, 'Выбери один из них!', reply_markup=markup)
 		elif message.text == 'Слитая инфаа!':
 			for i in range(8):
 				bot.send_message(message.chat.id, '<a href = "https://www.youtube.com'+ parser.href[i] + '">' + parser.title[i] + '</a>',
@@ -48,22 +51,19 @@ def pressMarkup(message):
 def callback_inline(call):
 	try:
 		if call.message:
-			if call.data == 'good':
-				#snd = open('/temp/nicedo.webp', 'rb')
-				#bot.send_sticker(chat_id=call.message.chat.id, sticker=snd)
-				bot.send_message(chat_id=call.message.chat.id, text='Прекрасно!')
-			elif call.data == 'bad':
-				#sbd = open('/temp/baddo.webp', 'rb')
-				#bot.send_sticker(chat_id=call.message.chat.id, sticker=sbd)
-				bot.send_message(chat_id=call.message.chat.id, text='Эт\' плохо.')
+			if call.data == project[0]:
+				bot.send_message(chat_id=call.message.chat.id, text='Вот он:\n<a href="https://github.com/Goodenl/'+ project[0] +'.git">'+ project[0] +'</a>', parse_mode="html")
 
-			#remove inline keyboard
-			bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Як справи, хлопче?",
-				reply_markup=None)
+			elif call.data == project[1]:
+				bot.send_message(chat_id=call.message.chat.id, text='Вот он:\n<a href="https://github.com/Goodenl/'+ project[1] +'.git">'+ project[1] +'</a>', parse_mode="html")
+
+			#remove inline keyboard and text
+			elif call.data == "close":
+				bot.delete_message( call.message.chat.id, call.message.message_id )
 
 			#show alert
-			bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
-				text="Push Alert!!!")
+				bot.answer_callback_query( callback_query_id=call.id, show_alert=False,
+					text="Push Alert!!!" )
 
 	except Exception as e:
 		print(repr(e))
